e8648a9e0a2740d158884f51bc61d01edf0dd9cb
Md. Mahmudul Hasan <69659701+antor9732@users.noreply.github.com>
2026-04-27 10:07:12 +0600

    Update CV download link in About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -76,7 +76,7 @@
           <!-- Download CV Button -->
           <div class="mt-4">
             <a
-              href="/public/Md_Mahmudul_Hasan_Wordpress_Cv.pdf"
+              href="/public/Mahmudul_Hasan_cv"
               class="btn btn-outline-light px-4 py-2 fw-semibold"
               download
             >
